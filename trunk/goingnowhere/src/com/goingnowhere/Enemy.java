0c5e25ec644ce05fd338bd4c0ccd1315717c1919
baterpruf@gmail.com <baterpruf@gmail.com@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-21 22:14:34 +0000

    Enemy

---
+++ trunk/goingnowhere/src/com/goingnowhere/Enemy.java
@@ -0,0 +1,18 @@
+package com.goingnowhere;
+
+public class Enemy extends DynamicGameObject {
+	public static final float ENEMY_WIDTH = 0.8f;
+	public static final float ENEMY_HEIGHT = 0.8f;
+	
+	public Enemy (float x, float y) {
+		super(x, y, ENEMY_WIDTH, ENEMY_HEIGHT);
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void die(){
+		
+	}
+}
