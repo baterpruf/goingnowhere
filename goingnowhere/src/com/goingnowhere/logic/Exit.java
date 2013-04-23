624c36ff0d0fe64f628ea5eaf3f4e65301c9ae73
baterpruf <baterpruf@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-23 10:08:56 +0000

    exit door

---
+++ goingnowhere/src/com/goingnowhere/logic/Exit.java
@@ -0,0 +1,15 @@
+package com.goingnowhere.logic;
+
+public class Exit extends GameObject {
+	public static final float EXIT_WIDTH = 0.2f;
+	public static final float EXIT_HEIGHT = 0.2f;
+	public static final int EXIT_SCORE = 1;
+	
+	public Exit (float x, float y) {
+		super(x, y, EXIT_WIDTH, EXIT_HEIGHT);
+	}
+	
+	public void update(float delta){
+		
+	}
+}
