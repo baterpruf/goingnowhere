aeb3204a510304bebe358b67040bc0f5cfc975dd
baterpruf <baterpruf@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-24 10:37:59 +0000

    detecting walls and floor

--- trunk/goingnowhere/src/com/goingnowhere/logic/DynamicGameObject.java
+++ trunk/goingnowhere/src/com/goingnowhere/logic/DynamicGameObject.java
@@ -6,12 +6,12 @@ public class DynamicGameObject extends GameObject {
 	static final int LEFT = -1;
 	static final int RIGHT = 1;
 	
-	public final Vector2 velocity;
+	public final Vector2 vel;
 	public final Vector2 accel;
 
 	public DynamicGameObject (float x, float y, float width, float height) {
 		super(x, y, width, height);
-		velocity = new Vector2();
+		vel = new Vector2();
 		accel = new Vector2();
 	}
 }
