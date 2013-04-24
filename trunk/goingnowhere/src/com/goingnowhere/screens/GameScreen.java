83d8668f2ec66e30a91847b39f4c29e1c10778e4
baterpruf <baterpruf@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-24 11:27:28 +0000

    trying to render

--- trunk/goingnowhere/src/com/goingnowhere/screens/GameScreen.java
+++ trunk/goingnowhere/src/com/goingnowhere/screens/GameScreen.java
@@ -1,10 +1,14 @@
 package com.goingnowhere.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.goingnowhere.WorldRenderer;
 import com.goingnowhere.logic.World;
 
 public class GameScreen extends BaseScreen {
 	World world;
+	WorldRenderer renderer;
 
 	public GameScreen (Game game) {
 		super(game);
@@ -13,11 +17,16 @@ public class GameScreen extends BaseScreen {
 	@Override
 	public void show () {
 		world = new World(1);
+		renderer = new WorldRenderer(world);
 	}
 
 	@Override
 	public void render (float delta) {
-
+		delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
+		world.update(delta);
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		renderer.render(delta);
 	}
 
 }
