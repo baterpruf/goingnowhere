4d19b243c39837115166246e1c54d72745100579
baterpruf <baterpruf@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-24 08:04:47 +0000

    Processing user input

--- trunk/goingnowhere/src/com/goingnowhere/logic/Hero.java
+++ trunk/goingnowhere/src/com/goingnowhere/logic/Hero.java
@@ -1,5 +1,7 @@
 package com.goingnowhere.logic;
 
+import com.badlogic.gdx.Gdx;
+
 public class Hero extends DynamicGameObject {
 	public static final float HERO_WIDTH = 0.8f;
 	public static final float HERO_HEIGHT = 0.8f;
@@ -20,10 +22,38 @@ public class Hero extends DynamicGameObject {
 	}
 	
 	public void update(float deltaTime){
+		processInput();
+		
 		velocity.add(World.gravity.x * deltaTime, World.gravity.y * deltaTime);
 		position.add(velocity.x * deltaTime, velocity.y * deltaTime);
 		bounds.x = position.x - bounds.width / 2;
 		bounds.y = position.y - bounds.height / 2;
+	}
+	
+	private void processInput(){
+		if (state == DEAD || state == DYING) return;
+		
+		//Admito hasta dos toques simultáneos, saltar y disparar.
+		float x0 = (Gdx.input.getX(0) / (float)Gdx.graphics.getWidth()) * 480;
+		float x1 = (Gdx.input.getX(1) / (float)Gdx.graphics.getWidth()) * 480;
+		float y0 = (Gdx.input.getY(0) / (float)Gdx.graphics.getHeight()) * 320;
+		float y1 = (Gdx.input.getY(1) / (float)Gdx.graphics.getHeight()) * 320;
+		
+		if(((x0<240 && y0>160) || (x1<240 && y1>160)) && state!=JUMP){
+			//Saltar
+			jump();
+			
+		}
+		if((x0>240 && y0>160) || (x1>240 && y1>160)){
+			//Disparar
+		}
+		if((x0<240 && y0<160) || (x1<240 && y1<160)){
+			//Dirección izquierda
+		}
+		if((x0>240 && y0<160) || (x1>240 && y1<160)){
+			//Dirección derecha
+		}
+		
 	}
 	
 	public void run(int direction){
