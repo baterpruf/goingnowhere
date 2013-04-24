acb0bd5ce35b23eb3bf2db07cafbf49143718288
baterpruf <baterpruf@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-24 14:34:51 +0000

    rendering

--- goingnowhere/src/com/goingnowhere/WorldRenderer.java
+++ goingnowhere/src/com/goingnowhere/WorldRenderer.java
@@ -1,6 +1,9 @@
 package com.goingnowhere;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteCache;
 import com.goingnowhere.logic.World;
@@ -9,14 +12,34 @@ public class WorldRenderer {
 	World world;
 	OrthographicCamera cam;
 	SpriteCache cache;
-	SpriteBatch batch = new SpriteBatch(10000);
+	SpriteBatch batch;
+	Texture heroImage;
+	Texture mapImage;
+	Texture backgroundImage;
 	
 	public WorldRenderer (World world) {
 		this.world = world;
 		this.cam = new OrthographicCamera(480, 320);
 		this.cam.position.set(world.hero.position.x, 160, 0);
+		heroImage = new Texture(Gdx.files.internal("data/hero.png"));
+		mapImage = new Texture(Gdx.files.internal("data/background1.png"));
+		backgroundImage = new Texture(Gdx.files.internal("data/background_buildings.png"));
+	    batch = new SpriteBatch();
+
+
 	}
 	public void render(float delta){
-		
+
+	      Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+	      Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+	      cam.update();
+	      batch.setProjectionMatrix(cam.combined);
+
+	      batch.begin();
+	      batch.draw(backgroundImage, -240,-192);
+	      batch.draw(mapImage, -240,-192);
+	      batch.draw(heroImage, world.hero.position.x*15-240, world.hero.position.y*15);
+	      batch.end();
 	}
 }
