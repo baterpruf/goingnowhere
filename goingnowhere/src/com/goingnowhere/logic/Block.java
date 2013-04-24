19079dcbf7885954a0c7ce55920dfc742c3bee91
baterpruf <baterpruf@1a3020cd-7596-f306-5792-0003b69c0ca7>
2013-04-24 17:02:22 +0000

    block size 20

--- goingnowhere/src/com/goingnowhere/logic/Block.java
+++ goingnowhere/src/com/goingnowhere/logic/Block.java
@@ -1,8 +1,8 @@
 package com.goingnowhere.logic;
 
 public class Block extends GameObject {
-	public static final float BLOCK_WIDTH = 0.2f;
-	public static final float BLOCK_HEIGHT = 0.2f;
+	public static final float BLOCK_WIDTH = 20f;
+	public static final float BLOCK_HEIGHT = 20f;
 	
 	public Block (float x, float y) {
 		super(x, y, BLOCK_WIDTH, BLOCK_HEIGHT);
